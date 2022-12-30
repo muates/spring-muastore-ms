ee6aacb9410a94244271ac5aef581b71ca46b13a
Murat Ateş <mrtt.atss@gmail.com>
2022-12-30 18:28:37 +0300

    refactoring

--- common-service/src/main/java/com/muates/commonservice/model/BaseEntity.java
+++ common-service/src/main/java/com/muates/commonservice/model/BaseEntity.java
@@ -5,13 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.util.Date;
 
+@MappedSuperclass
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class BaseEntity {
+public class BaseEntity implements Serializable {
     private Date createdDate;
     private Date updatedDate;
 }
