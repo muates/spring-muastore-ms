d7e6661100f621ab3e25967a189e51d32bf5763d
Murat Ateş <mrtt.atss@gmail.com>
2023-01-06 22:48:06 +0300

    removed

--- notification-service/src/main/java/com/muates/notificationservice/service/EmailActivationService.java
+++
@@ -1,9 +0,0 @@
-package com.muates.notificationservice.service;
-
-import com.muates.notificationservice.model.dto.UserActivationRequest;
-
-public interface EmailActivationService {
-    void sendEmail(UserActivationRequest request);
-    void updateEnable(Long userId);
-    Boolean isEnable(Long userId);
-}
