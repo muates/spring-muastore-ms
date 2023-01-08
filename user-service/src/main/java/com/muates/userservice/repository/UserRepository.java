1194d9fa186d2797be505643fffdbea848c48eba
Murat Ateş <mrtt.atss@gmail.com>
2023-01-08 12:52:07 +0300

    find user by email added

--- user-service/src/main/java/com/muates/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/muates/userservice/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.muates.userservice.repository;
 import com.muates.userservice.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+    Optional<User> findUserByEmail(String email);
 
     @Query(
             value = "SELECT * FROM users u " +
